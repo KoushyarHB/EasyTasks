7495f6069d657fe4e2aa8cff24a1134ef5c12952
Koushyar <koushyarheidaribateni@gmail.com>
2025-03-23 20:03:37 +0330

    2-23 Required & Optional Inputs

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -7,8 +7,9 @@ import { Component, Input } from '@angular/core';
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  @Input() avatar!: string;
-  @Input() name!: string;
+  // inputs are also properties of the class component and we use property binding to pass them to our components
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) name!: string;
 
   get imagePath(): string {
     return 'users/' + this.avatar;
