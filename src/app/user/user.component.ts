38efb4c7ec83c53f4ed463d8c5a04898032ffcac
Koushyar <koushyarheidaribateni@gmail.com>
2025-03-23 20:48:31 +0330

    2-24 Using Signal Inputs

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, computed, input, Input } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -8,12 +8,23 @@ import { Component, Input } from '@angular/core';
 })
 export class UserComponent {
   // inputs are also properties of the class component and we use property binding to pass them to our components
-  @Input({ required: true }) avatar!: string;
-  @Input({ required: true }) name!: string;
+  // @Input({ required: true }) avatar!: string;
+  // @Input({ required: true }) name!: string;
 
-  get imagePath(): string {
-    return 'users/' + this.avatar;
+  // Signal input definition
+  avatar = input.required<string>();
+  name = input.required<string>();
+
+  // Signal input usage
+  // get imagePath(): string {
+  //   return 'users/' + this.avatar();
+  // }
+
+  // Computed Signal input definition
+  imagePath = computed(() => 'users/' + this.avatar());
+
+  onSelectUser() {
+    // Signal input can not be set from inside the component
+    // this.name.set()
   }
-
-  onSelectUser() {}
 }
