2435fec683e6152bb72c15eb536c935c16d0b790
Koushyar <koushyarheidaribateni@gmail.com>
2025-03-24 23:03:03 +0330

    2-29 Exercise Create a Configurable Component

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -19,8 +19,7 @@ export class UserComponent {
   @Input({ required: true }) id!: string;
   @Input({ required: true }) avatar!: string;
   @Input({ required: true }) name!: string;
-  // @Output() select = new EventEmitter<string>();
-  select = output<string>();
+  @Output() select = new EventEmitter<string>();
 
   get imagePath(): string {
     return 'users/' + this.avatar;
